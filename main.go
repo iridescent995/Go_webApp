25375939b6d6d53e4c28196af3f62339ef07cc1b
Harshit <ht2995@gmail.com>
2018-09-01 21:23:09 +0530

    files

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import ("fmt"
-"io/ioutil"
-"net/http")
+import (
+"encoding/xml"
+  "fmt"
+  "io/ioutil"
+  "net/http")
+
+type Sitemapindex struct{
+  Locations []Location `xml:"sitemap"`
+}
+
+type Location struct{
+  Loc string `xml:"loc"`
+}
+
+func (l Location) String() string{
+  return fmt.Sprintf(l.Loc)
+}
 
 func main() {
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	string_body := string(bytes)
-	fmt.Println(string_body)
-	resp.Body.Close()
+  resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+  bytes, _ := ioutil.ReadAll(resp.Body)
+  var s Sitemapindex
+  xml.Unmarshal(bytes,&s)
+  fmt.Println(s.Locations)
+  //resp.Body.Close()
 }
