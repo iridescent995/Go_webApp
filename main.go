07c533830180ad68cff58a1ba239455153d8a306
Harshit <ht2995@gmail.com>
2018-09-01 21:44:38 +0530

    file

--- main.go
+++ main.go
@@ -10,16 +10,26 @@ type Sitemapindex struct{
   Locations []string `xml:"sitemap>loc"`
 }
 
-
+type News struct{
+	Titles [] string `xml:"url>n:news>n:title"`
+	Keywords []string `xml:"url>n:news>n:keywords"`
+	Locations []string `xml:"url>loc"`
+}
 func main() {
+  var s Sitemapindex
+  var n News
+
   resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
   bytes, _ := ioutil.ReadAll(resp.Body)
-  var s Sitemapindex
+  
   xml.Unmarshal(bytes,&s)
   //fmt.Println(s.Locations)
 
   for _, Location := range(s.Locations){
-  	fmt.Println(Location)
+  	resp, _ := http.Get(Location)
+  	bytes, _ := ioutil.ReadAll(resp.Body)
+  	xml.Unmarshal(bytes,&n)
+  	fmt.Println(n.Locations)
   }
   //resp.Body.Close()
 }
