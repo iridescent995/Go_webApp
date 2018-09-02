a511f2bcbeab7ddae443550cfdf192168cc4bdc9
Harshit <ht2995@gmail.com>
2018-09-02 11:58:17 +0530

    files:

--- main.go
+++ main.go
@@ -6,19 +6,28 @@ import (
   "io/ioutil"
   "net/http")
 
+
+//slice of location of sitemaps depending on different topics from main site map
 type Sitemapindex struct{
   Locations []string `xml:"sitemap>loc"`
 }
+
+
 
 type News struct{
 	Titles [] string `xml:"url>n:news>n:title"`
 	Keywords []string `xml:"url>n:news>n:keywords"`
 	Locations []string `xml:"url>loc"`
 }
+
+
+
 func main() {
   var s Sitemapindex
   var n News
 
+
+  //Obtaining sitemap data from washington post
   resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
   bytes, _ := ioutil.ReadAll(resp.Body)
   
@@ -31,5 +40,7 @@ func main() {
   	xml.Unmarshal(bytes,&n)
   	fmt.Println(n.Locations)
   }
+
+  
   //resp.Body.Close()
 }
