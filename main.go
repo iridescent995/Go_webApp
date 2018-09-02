8c500d176cb2f2005a7fd0df8a811f91d4d18b45
Harshit <ht2995@gmail.com>
2018-09-02 12:55:28 +0530

    files:

--- main.go
+++ main.go
@@ -13,19 +13,26 @@ type Sitemapindex struct{
 }
 
 
+//extracting data like titles keywords locations from each xml sitemap
+type News struct {
+  Titles []string `xml:"url>news>title"`
+  Keywords []string `xml:"url>news>keywords"`
+  Locations []string `xml:"url>loc"`
+}
 
-type News struct{
-	Titles [] string `xml:"url>n:news>n:title"`
-	Keywords []string `xml:"url>n:news>n:keywords"`
-	Locations []string `xml:"url>loc"`
+//putting news in a map
+type NewsMap struct {
+  Keyword string
+  Location string
 }
 
 
 
 func main() {
+
   var s Sitemapindex
   var n News
-
+  news_map := make(map[string]NewsMap)
 
   //Obtaining sitemap data from washington post
   resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
@@ -38,9 +45,23 @@ func main() {
   	resp, _ := http.Get(Location)
   	bytes, _ := ioutil.ReadAll(resp.Body)
   	xml.Unmarshal(bytes,&n)
-  	fmt.Println(n.Locations)
+  	//fmt.Println(n.Locations)
+
+    //storing data in a map
+    for idx, _ := range n.Keywords {
+      news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+    }
+
   }
 
-  
+  //fmt.Println("reached")
+
+  for idx, data := range news_map {
+    fmt.Println("\n\n\n",idx)
+    fmt.Println("\n",data.Keyword)
+    fmt.Println("\n",data.Location)
+  }
+
+
   //resp.Body.Close()
 }
